c5e66824d3ac4f92a0cc73f87d36af6a45870dfe
anthonybdev <anton.bitner.kn.2021@gmail.com>
2021-10-24 00:44:34 +0300

    initialized vue cli project, added starting page, some changes to readme

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img class="imgLogo" alt="logo" src="../assets/logo.jpg" />
+    <StartingPage msg="BEST Lviv starting page" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import StartingPage from "@/components/StartingPage.vue";
+
+export default {
+  name: "Home",
+  components: {
+    StartingPage,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.imgLogo
+  width: 250px;
+  height: auto;
+</style>
